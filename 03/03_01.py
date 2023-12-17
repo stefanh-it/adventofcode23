b97fde6a57e81a665b3ef6b6e7a320d3ff6b0a53
Stefan <chinaxsteve@gmail.com>
2023-12-17 09:01:48 +0100

    refactor the character games array

--- 03/03_01.py
+++ 03/03_01.py
@@ -13,7 +13,6 @@ def get_coordinates(text_lines):
     previous_line = []
     next_line = []
     character_games = []
-    character_game = []
     for index, text_line in enumerate(text_lines):
         line_string = None
         #Check if index is greater than 0
@@ -31,93 +30,185 @@ def get_coordinates(text_lines):
         # print(f"next_line: {next_line}")
 
         for i, j in enumerate(text_line):
+            character_game = []
             char = j
             y_coord = text_lines.index(text_line)+1
             coordinate = [char, [i, y_coord]]
             coordinates.append(coordinate)
-            # Check if character is not first character in line and not in first or last line
-            if i == 0 and None not in (previous_line, next_line):
-                top_char = previous_line[i]
+            top_char = ""
+            top_right_char = ""
+            top_left_char = ""
+            bottom_char = ""
+            bottom_right_char = ""
+            bottom_left_char = ""
+            left_char = ""
+            right_char = ""
+            # Check if character is in first line
+            if previous_line is None:
+                top_char = ""
+                top_right_char = ""
+                top_left_char = ""
+                # Check if character is first character in line in first line
+                if i == 0:
+                    left_char = ""
+                    bottom_left_char = ""
+                    right_char = text_line[i+1]
+                    bottom_right_char = next_line[i+1]
+                    bottom_char = next_line[i]
+                # Check if character is last character in line in first line
+                elif i == len(text_line)-1:
+                    left_char = text_line[i-1]
+                    bottom_left_char = next_line[i-1]
+                    right_char = ""
+                    bottom_right_char = ""
+                    bottom_char = next_line[i]
+                # Check if any character in in between
+                elif i > 0 and i < len(text_line)-1:
+                    left_char = text_line[i-1]
+                    bottom_left_char = next_line[i-1]
+                    right_char = text_line[i+1]
+                    bottom_right_char = next_line[i+1]
+                    bottom_char = next_line[i]
+            elif next_line is None:
+                bottom_char = ""
+                bottom_left_char = ""
+                bottom_right_char = ""
+                # Check if character is first character in line in last line
+                if i == 0:
+                    left_char = ""
+                    top_left_char = ""
+                    right_char = text_line[i+1]
+                    top_right_char = previous_line[i+1]
+                    top_char = previous_line[i]
+                # Check if character is last character in line in last line
+                elif i == len(text_line)-1:
+                    left_char = text_line[i-1]
+                    top_left_char = previous_line[i-1]
+                    right_char = ""
+                    top_right_char = ""
+                    top_char = previous_line[i]
+                elif i > 0 and i < len(text_line)-1:
+                    left_char = text_line[i-1]
+                    top_left_char = previous_line[i-1]
+                    right_char = text_line[i+1]
+                    top_right_char = previous_line[i+1]
+                    top_char = previous_line[i]
+            # Check if character is not in first or last line
+            elif next_line and previous_line:
                 bottom_char = next_line[i]
-                left_char = None
-                top_left_char = None
-                bottom_left_char = None
-                right_char = text_line[i+1]
-                top_right_char = previous_line[i+1]
-                bottom_right_char = next_line[i+1]
-            # Check is character is last in line
-            elif i == len(text_line)-1 and None not in (previous_line, next_line):
                 top_char = previous_line[i]
-                bottom_char = next_line[i]
-                left_char = text_line[i-1]
-                top_left_char = previous_line[i-1]
-                bottom_left_char = next_line[i-1]
-                right_char = None
-                top_right_char = None
-                bottom_right_char = None
-            # Check if character is first character in line of the first line
-            elif i == 0 and previous_line is None:
-                top_char = None
-                bottom_char = next_line[i]
-                left_char = None
-                top_left_char = None
-                bottom_left_char = None
-                right_char = text_line[i+1]
-                top_right_char = None
-                bottom_right_char = next_line[i+1]
-            # Check if character is last character in line of the first line
-            elif i == len(text_line)-1 and previous_line is None:
-                top_char = None
-                bottom_char = next_line[i]
-                left_char = text_line[i-1]
-                top_left_char = None
-                bottom_left_char = next_line[i-1]
-                right_char = None
-                top_right_char = None
-                bottom_right_char = None
-            # Check if character is first character in line of the last line
-            elif i == 0 and next_line is None:
-                top_char = previous_line[i]
-                top_right_char = previous_line[i+1]
-                right_char = text_line[i+1]
-                bottom_right_char = None
-                bottom_char = None
-                bottom_left_char = None
-            # Check if character is last character in line of the last line
-            elif i == len(text_line)-1 and next_line is None:
-                top_char = previous_line[i]
-                top_left_char = previous_line[i-1]
-                left_char = text_line[i-1]
-                bottom_left_char = None
-                bottom_char = None
-                bottom_right_char = None
-            elif i > 0 and i < len(text_line)-1 and None not in (previous_line, next_line):
-                top_char = previous_line[i]
-                bottom_char = next_line[i]
-                left_char = text_line[i-1]
-                top_left_char = previous_line[i-1]
-                bottom_left_char = next_line[i-1]
-                right_char = text_line[i+1]
-                top_right_char = previous_line[i+1]
-                bottom_right_char = next_line[i+1]
+                # Check if character is first in line
+                if i == 0:
+                    left_char = ""
+                    top_left_char = ""
+                    bottom_left_char = ""
+                    right_char = text_line[i+1]
+                    top_right_char = previous_line[i+1]
+                    bottom_right_char = next_line[i+1]
+                # Check if character is last in line
+                elif i == len(text_line)-1:
+                    left_char = text_line[i-1]
+                    top_left_char = previous_line[i-1]
+                    bottom_left_char = next_line[i-1]
+                    right_char = ""
+                    top_right_char = ""
+                    bottom_right_char = ""
+                # Check if character is in between
+                elif i > 0 and i < len(text_line)-1:
+                    left_char = text_line[i-1]
+                    top_left_char = previous_line[i-1]
+                    bottom_left_char = next_line[i-1]
+                    right_char = text_line[i+1]
+                    top_right_char = previous_line[i+1]
+                    bottom_right_char = next_line[i+1]
             character_game = [char,
-                              [top_char, top_right_char, right_char,
-                               bottom_right_char, bottom_char,
-                               bottom_left_char, left_char,
-                               top_left_char]]
+                              [right_char, top_right_char, bottom_right_char,
+                               bottom_char, bottom_left_char, left_char,
+                               top_left_char, top_char]]
             character_games.append(character_game)
+        #return previous_line, text_line, next_line
+    print(f"character_games: {character_games}")
+
     return coordinates, character_games
 
-def evaluate_games(character_games):
+def evaluate_games(character_games, text_lines):
     valid_numbers = []
-    for character_game in character_games:
+    digit = []
+    digits = []
+    valid_digit = []
+    valid_digits = []
+    for i, character_game in enumerate(character_games):
         character = ""
         character = str(character_game[0])
         set = character_game[1]
-        #print(f"{set}")
-        validators = ['.', '#', '*', '+', '$', '=', '/', '@', '-']
-        if character.isdigit() == True: 
-            print(f"char {character}, isdigit")
+        validators = ['#', '\*', '+', '$', '=', '/', '@', '-']
+        
+        # Conditions
+        # any of the validators in set
+        # set[0] = right_char
+        # set[5] = left_char
+        
+        # Check if character is a digit
+        if character.isdigit() is True \
+                and any(x in set for x in validators) is False:
+            digit.append(character)
+        # Check if char is a digit and and a valid digit 
+            if character.isdigit() is True \
+                    and any(x in set for x in validators) is True:
+                valid_digit.append(character)
+            # Check if char is a digit, not first in line and part of a valid number
+            if valid_digit:
+                if character.isdigit() is True \
+                        and (i % 10 != 0) \
+                        and set[5] == valid_digit[-1]:
+                    valid_digit.append(character)
+            # Check if  char is a valid digit and part of a number
+            if digit:
+                if character.isdigit() is True \
+                        and set[5] == digit[-1] \
+                        and any(x in set for x in validators) is True:
+                    digit.append(character)
+                    valid_digit.append(digit)
+            # Check if char terminates a number
+        if character.isdigit() is False and \
+                set[5].isdigit() is True:
+            valid_digits.append(valid_digit)
+            digits.append(digit)
+            digit.clear()
+            valid_digit.clear()
+
+        if (i+1) % 10 == 0 and i > 0 and valid_digit is not None and digit is not None:
+            valid_digits.append(valid_digit)
+            digits.append(digit)
+            digit.clear()
+            valid_digit.clear()
+
+    valid_numbers.append(valid_digits)
+            # #elif character.isdigit() is False and i+1 > 1\
+            #         and (set[1] == next(reversed(valid_numbers)) \
+            #         or set[1] == next(reversed(numbers))):
+            #     valid_numbers.append(valid_number)
+
+
+            # Check if previous char set[5] is the last value character_games[i-1].character_game[0]
+            # if set[5] == character_games[i-1][0] and character.isdigit() is True:
+            #     current_valid_number = current_valid_number + str(character)
+            #     print(current_valid_number)
+            #     break
+            #elif character.isdigit() is True and set[5].isdigit() is True
+    print(f"valid_numbers: {valid_numbers}")
+                # print(f"{text_line}")
+        # match_count = 0
+        # numbers_in_line = []
+        # for match in re.finditer(r'\d+', text_line):
+        #     print(f"match: {match}")
+        #     if match is None:
+        #         numbers_in_line = []
+        #     else:
+        #         numbers_in_line.append(match)
+        #         number_start_position = match.start()
+        #         number_end_position = match.end()
+            #print(f"{set}")
         #print(f"character: {character}, set: {set}")
             # if character == '.':
             #     print(f"dot {character}")
@@ -154,7 +245,79 @@ def evaluate_games(character_games):
     return coordinates
 
 coordinates, character_games = (list(get_coordinates(text_lines)))
-valid_numbers = evaluate_games(character_games)
+valid_numbers = evaluate_games(character_games, text_lines)
 #print(character_games)
 #evaluate_coordinates(coordinates)
 #print(coordinates)
+
+
+### Old 
+
+#"""             # Check if character is not first character in line and not in first or last line
+#            if i == 0 and None not in (previous_line, next_line):
+#                top_char = previous_line[i]
+#                bottom_char = next_line[i]
+#                left_char = ""
+#                top_left_char = ""
+#                bottom_left_char = "" 
+#                right_char = text_line[i+1]
+#                top_right_char = previous_line[i+1]
+#                bottom_right_char = next_line[i+1]
+#                print(char)
+#            # Check is character is last in line
+#            elif i == len(text_line)-1 not in (previous_line, next_line):
+#                top_char = previous_line[i]
+#                bottom_char = next_line[i]
+#                left_char = text_line[i-1]
+#                top_left_char = previous_line[i-1]
+#                bottom_left_char = next_line[i-1]
+#                right_char = ""
+#                top_right_char = ""
+#                bottom_right_char = ""
+#            # Check if character is first character in line of the first line
+#            elif i == 0 and previous_line is None:
+#                top_char = ""
+#                bottom_char = next_line[i]
+#                left_char = ""
+#                top_left_char = ""
+#                bottom_left_char = ""
+#                right_char = text_line[i+1]
+#                top_right_char = ""
+#                bottom_right_char = next_line[i+1]
+#            # Check if character is last character in line of the first line
+#            elif i == len(text_line)-1 and previous_line is None:
+#                top_char = ""
+#                bottom_char = next_line[i]
+#                left_char = text_line[i-1]
+#                top_left_char = ""
+#                bottom_left_char = next_line[i-1]
+#                right_char = ""
+#                top_right_char = ""
+#                bottom_right_char = ""
+#            # Check if character is first character in line of the last line
+#            elif i == 0 and next_line is None:
+#                top_char = previous_line[i]
+#                top_right_char = previous_line[i+1]
+#                right_char = text_line[i+1]
+#                bottom_right_char = ""
+#                bottom_char = ""
+#                bottom_left_char = ""
+#            # Check if character is last character in line of the last line
+#            elif i == len(text_line)-1 and next_line is None:
+#                top_char = previous_line[i]
+#                top_left_char = previous_line[i-1]
+#                left_char = text_line[i-1]
+#                bottom_left_char = ""
+#                bottom_char = ""
+#                bottom_right_char = ""
+#            elif i > 0 and i < len(text_line)-1 and None not in (previous_line, next_line):
+#                top_char = previous_line[i]
+#                bottom_char = next_line[i]
+#                left_char = text_line[i-1]
+#                top_left_char = previous_line[i-1]
+#                bottom_left_char = next_line[i-1]
+#                right_char = text_line[i+1]
+#                top_right_char = previous_line[i+1]
+#                bottom_right_char = next_line[i+1] 
+#                """
+#
