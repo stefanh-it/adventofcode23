14f89b0fb1426c447258a4fc6e00b54a745ba0d7
Stefan <chinaxsteve@gmail.com>
2023-12-31 06:54:52 +0100

    finished part 02 of day 6. easy

---
+++ 06/p2.py
@@ -0,0 +1,24 @@
+def raceBoats(time, target_dist):
+    travel_distance = 0
+    valid_game = 0
+    time = int(time)
+    for speed in range(time):
+        speed += 1
+        travel_distance = (time-speed) * speed
+        #print(f"Speed: {speed}", f"Time: {time}", f"Distance: {travel_distance}")
+        if travel_distance > int(target_dist):
+            valid_game += 1
+        #print(f"Valid Games: {valid_game}")
+    return valid_game
+
+
+def main(input):
+    input = input.splitlines()
+    time = ''.join(input[0].split()[1:])
+    target_dist = ''.join(input[1].split()[1:])
+    solution = raceBoats(time, target_dist)
+    return solution, None
+
+if __name__ == '__main__':
+    main(input)
+
